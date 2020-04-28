7cd6ffda89ef23a11bf26223544b986ac8b5f86e
AMTazi <taziabdelmounaim@gmail.com>
2020-04-29 00:35:37 +0100

    Feature: Clean up unnecessary files.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import Vue from "vue";
-import './plugins/bootstrap-vue'
 import App from "./App.vue";
+import "./plugins/bootstrap-vue";
 import router from "./router";
 import store from "./store";
 
